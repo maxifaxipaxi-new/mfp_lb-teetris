a4ff42a82cde5c579f9b1af6fc34c378e7770f2b
MaxiFaxiPaxi <115405418+maxifaxipaxi-new@users.noreply.github.com>
2024-07-09 16:39:13 +0200

    Add files via upload

---
+++ mfp_lb-teetris/config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+Config.name = "Classic Teetris '84" -- Name of the App
+Config.description = "Relive the Retro Fun!" -- disc in the appstore (edit for your local)
+Config.defaultApp = false -- OPTIONAL if set to true, app should be added without having to download it,
+Config.size = 4932 -- OPTIONAL in kb
+Config.price = 0 -- OPTIONAL, Make players pay with in-game money to download the app
+
+
+-- Support on discord.mfpscripts.com if needed. Check out issues on github. --
